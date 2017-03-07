4f270f42708745e810eca2938f7075181f1bd9b2
langmanleguang <langmanleguang@qq.com>
2017-03-07 18:12:17 +0800

    修改demo

--- app/src/main/java/cn/itsite/abase/demo/MainActivity.java
+++ app/src/main/java/cn/itsite/abase/demo/MainActivity.java
@@ -3,15 +3,15 @@ package cn.itsite.abase.demo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import cn.itsite.abase.cache.MemoryCache;
 import cn.itsite.abase.log.ALog;
@@ -48,6 +48,12 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
         ALog.e(TAG);
         MemoryCache.getInstance().clear();
+
+
+
+
+
+
 
 
     }
